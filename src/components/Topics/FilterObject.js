815f09f6d06b923877bb477ef7c90f6b3e45fa4c
Josiah Maina <josiah5115@gmail.com>
2018-06-29 18:32:01 -0600

    afternoon project

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FilterObject extends Component {
+  render() {
+    return (
+      <p>Aloha!! From FilterObject Component!</p>
+    )
+  }
+}
+
+export default FilterObject;
---
+++ src/components/Topics/Sum.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Sum extends Component {
+  render() {
+    return (
+      <p>Yah-Man!! From Sum Component!</p>
+    )
+  }
+}
+
+export default Sum;
